341c7a17a4d36494f7bd0a7d72c06289a7734867
natoine <antoineseilles@gmail.com>
2016-09-04 18:42:55 +0200

    added a remove method

--- world.js
+++ world.js
@@ -13,4 +13,10 @@ class World {
 		}
 		//return this.collection().filter(lending => lending.state === query)
 	}
+	remove(name) {
+		//récupérer la liste des emprunts dont le nom n'est pas celui passé en paramètre
+		const lendings = this.collection().filter( lending => lending.name !== name )
+		//maintenant notre liste d'emprunts locale est cette liste filtrée
+		localStorage.setItem('lendings', JSON.stringify(lendings))
+	}
 }
