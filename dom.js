576001931a0956be4c0a4468f93535285add74a9
natoine <antoineseilles@gmail.com>
2016-09-14 18:34:12 +0200

    created method displayLendings in dom.js

--- dom.js
+++ dom.js
@@ -7,3 +7,12 @@ Dom.displayFriends = (friends) => {
 		document.querySelector('#friends').appendChild(li)
 	})
 }
+
+Dom.displayLendings = (lendings) => {
+	lendings.map(lending => {
+		const li = document.createElement('li')
+		//way to add remove button seems to be dirty to me ... shouldn't I use the dom and addElement ?
+		li.innerHTML = `${lending.name} emprunté par ${lending.friend} depuis le ${lending.date} <button type="button" onclick="remove('${lending.name}')">Remove</button>`
+		document.querySelector('#lendings').appendChild(li)
+	})
+}
