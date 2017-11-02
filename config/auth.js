b8b4ef1f2abb8d153e3889f91bb328bf952c5241
natoine <antoineseilles@gmail.com>
2017-11-02 19:24:43 +0100

    re create a profile ejs ...

--- config/auth.js
+++ config/auth.js
@@ -5,20 +5,20 @@
 module.exports = {
 
     'facebookAuth' : {
-        'clientID'      : '', // your App ID
-        'clientSecret'  : '', // your App Secret
+        'clientID'      : '1079544955481974', // your App ID
+        'clientSecret'  : 'ff01fca2fde21f39ddc721c8c120edba', // your App Secret
         'callbackURL'   : 'http://localhost:8080/auth/facebook/callback'
     },
 
     'twitterAuth' : {
-        'consumerKey'       : '',
-        'consumerSecret'    : '',
+        'consumerKey'       : 'lyvHGhVhl4BeKgcPnnFp70tEM',
+        'consumerSecret'    : 'wK7883X7JAZYqwSI4X3F0Ib1PFj4t136J2jv5pSXu3furUIJd7',
         'callbackURL'       : 'http://localhost:8080/auth/twitter/callback'
     },
 
     'googleAuth' : {
-        'clientID'      : '',
-        'clientSecret'  : '',
+        'clientID'      : '874888543519-a8afvjr34eefjvilogc14qs06b99jjip.apps.googleusercontent.com',
+        'clientSecret'  : 'cIzip__4Gtqio5vEqRvgiPth',
         'callbackURL'   : 'http://localhost:8080/auth/google/callback'
     }
 
