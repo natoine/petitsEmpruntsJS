b9abc61b7acead8d8128cadafb7070f1d58dcd1a
natoine <antoineseilles@gmail.com>
2017-11-03 12:44:20 +0100

    removed any trace of twitter

--- application/routes.js
+++ application/routes.js
@@ -379,20 +379,7 @@ module.exports = function(app, passport) {
             successRedirect : '/main',
             failureRedirect : '/'
         }))
-/*
-// =====================================
-    // TWITTER ROUTES ======================
-    // =====================================
-    // route for twitter authentication and login
-    app.get('/auth/twitter', passport.authenticate('twitter'))
 
-    // handle the callback after twitter has authenticated the user
-    app.get('/auth/twitter/callback',
-        passport.authenticate('twitter', {
-            successRedirect : '/main',
-            failureRedirect : '/'
-        }))
-*/
  // =====================================
     // GOOGLE ROUTES =======================
     // =====================================
--- config/passport.js
+++ config/passport.js
@@ -3,7 +3,6 @@
 // load all the things we need
 var LocalStrategy   = require('passport-local').Strategy
 var FacebookStrategy = require('passport-facebook').Strategy
-var TwitterStrategy  = require('passport-twitter').Strategy
 var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy
 
 //to send emails
@@ -274,94 +273,6 @@ module.exports = function(passport)
         })
     }))
 ////end Facebook
-
-// =========================================================================
-    // TWITTER =================================================================
-    // =========================================================================
-/*    passport.use(new TwitterStrategy({
-
-        consumerKey     : configAuth.twitterAuth.consumerKey,
-        consumerSecret  : configAuth.twitterAuth.consumerSecret,
-        passReqToCallback : true, // allows us to pass in the req from our route (lets us check if a user is logged in or not)
-        callbackURL     : configAuth.twitterAuth.callbackURL
-
-    },
-    function(req, token, tokenSecret, profile, done) {
-
-        // make the code asynchronous
-    // User.findOne won't fire until we have all our data back from Twitter
-        process.nextTick(function() {
-
-            // check if the user is already logged in
-            if (!req.user) {
-
-            User.findOne({ 'twitter.id' : profile.id }, function(err, user) {
-
-                // if there is an error, stop everything and return that
-                // ie an error connecting to the database
-                if (err)
-                    return done(err)
-
-                // if the user is found then log them in
-                if (user) {
-                    // if there is a user id already but no token (user was linked at one point and then removed)
-                        // just add our token and profile information
-                        if (!user.twitter.token) {
-                            user.twitter.token = token
-                            user.twitter.username  = profile.username
-                            user.twitter.displayname = profile.displayName
-                            
-                            user.save(function(err) {
-                                if (err)
-                                    throw err
-                                return done(null, user)
-                            })
-                        }
-
-                        return done(null, user) // user found, return that user
-                } else {
-                    // if there is no user, create them
-                    var newUser                 = new User()
-                    console.log(profile)
-
-                    // set all of the user data that we need
-                    newUser.twitter.id          = profile.id
-                    newUser.twitter.token       = token
-                    newUser.twitter.username    = profile.username
-                    newUser.twitter.displayName = profile.displayName
-                    newUser.local.mailvalidated = true
-
-                    // save our user into the database
-                    newUser.save(function(err) {
-                        if (err)
-                            throw err
-                        return done(null, newUser)
-                    })
-                }
-            })
-
-        } else {
-            // user already exists and is logged in, we have to link accounts
-                var user            = req.user // pull the user out of the session
-
-                // update the current users facebook credentials
-                user.twitter.id    = profile.id
-                user.twitter.token = token
-                user.twitter.username  = profile.username // look at the passport user profile to see how names are returned
-                user.twitter.displayName  = profile.displayName
-                // save the user
-                user.save(function(err) {
-                    if (err)
-                        throw err
-                    return done(null, user)
-                })
-        }
-
-        })
-
-    }))
-*/
-//// end Twitter
 
 //// Google
 // =========================================================================
