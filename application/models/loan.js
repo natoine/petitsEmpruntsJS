7d9711c1ab787411e9ef63d0b7b0207dfc419ecd
natoine <antoineseilles@gmail.com>
2017-11-17 17:43:53 +0100

    now display a duration since the beginning of the loan

--- application/models/loan.js
+++ application/models/loan.js
@@ -2,6 +2,9 @@
 // load the things we need
 const mongoose = require('mongoose')
 
+const moment = require('moment')
+moment().format()
+
 // define the schema for our user model
 const loanSchema = mongoose.Schema({
 	loaner		: String, //idealy mail 
@@ -12,7 +15,11 @@ const loanSchema = mongoose.Schema({
 })
 
 // methods ======================
-
+loanSchema.methods.getDelay = function() 
+{
+	moment.locale('fr')
+	return moment(this.when, "D MMM YYYY").fromNow()
+}
 // create the model for loans and expose it to our app
 const configDB = require('../../config/database.js')
 const db = mongoose.createConnection(configDB.url)
