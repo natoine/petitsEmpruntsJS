3933d83c2e7451074bd61020adc7d829c3e798d9
natoine <antoineseilles@gmail.com>
2017-11-25 21:59:40 +0100

    now view manage error message when try to change username

--- application/routes/routes.js
+++ application/routes/routes.js
@@ -518,7 +518,8 @@ module.exports = function(app, passport) {
     // we will use route middleware to verify this (the isLoggedIn function)
     app.get('/user/:username', isLoggedInAndActivated, function(req, res) {
         res.render('profile', {
-            user : req.user // get the user out of session and pass to template
+            user : req.user, // get the user out of session and pass to template
+            message : req.flash('messageusername')
         })
     })
 
