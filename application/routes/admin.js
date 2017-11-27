bf6ed510c19c2583628babbf8afc8f14a21d9ec5
natoine <antoineseilles@gmail.com>
2017-11-27 20:43:19 +0100

    now get a page for loans but need to show something

--- application/routes/admin.js
+++ application/routes/admin.js
@@ -1,13 +1,15 @@
 const security = require('../utils/securityMiddleware')
 // load up the user model
 const User            = require('../models/user')
+//load up the loan model
+const Loan = require('../models/loan')
 
 module.exports = function(app, express) {
 
     // get an instance of the router for main routes
     const adminRoutes = express.Router()
 
-    adminRoutes.get('/admin', security.isSuperAdmin, function(req, res) {
+    adminRoutes.get('/users', security.isSuperAdmin, function(req, res) {
     	User.find( {} ,
     		{
 	    		"local.username" : true,
@@ -29,7 +31,18 @@ module.exports = function(app, express) {
     	)
     })
 
+    adminRoutes.get('/loans', security.isSuperAdmin, function(req, res) {
+    	Loan.find( {} , {} , function(err, loans) {
+    			if(err) throw err
+            	else 
+               	{
+               		res.render('admin/loans' , 
+               			{username : req.user.local.username, loans : loans})
+               	}
+    		})
+    })
+
     // apply the routes to our application
-    app.use('/', adminRoutes)
+    app.use('/admin', adminRoutes)
 
 }
